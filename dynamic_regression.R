7891da5253b2db9b5cf66951898fc3aa62b02be4
LuisArmandoPerez <kumano_1@hotmail.com>
2017-07-25 11:04:14 -0500

    Add modifications to dynamical

--- dynamic_regression.R
+++ dynamic_regression.R
@@ -55,12 +55,26 @@ ggtsdisplay(gdp_sweden.ts.s1)
 ener_sweden.proxy <- Arima(ener_sweden.ts.s1, xreg=gdp_sweden.ts.s1,  order = c(2,0,0))
 ener_sweden.proxy
 tsdisplay(residuals(ener_sweden.proxy))
+par(mar = rep(2, 4))
+
+shapiro.test(residuals(ener_sweden.proxy))
+
+Box.test(residuals(ener_sweden.proxy),lag=12,type="Ljung-Box")
+accuracy
 
 #Automatic dynamic regression
 ener_sweden.auto <- auto.arima(ener_sweden.ts.s1, xreg=gdp_sweden.ts.s1)
 ener_sweden.auto
 tsdisplay(residuals(ener_sweden.auto))
-
+shapiro.test(residuals(ener_sweden.auto))
+Box.test(residuals(ener_sweden.auto),lag=12,type="Ljung-Box")
+names(ener_sweden.auto)
+plot(ener_sweden.auto$xreg)
+ener_sweden.dyn<-diffinv(ener_sweden.auto$xreg,xi = log(2.700534),differences = 1)
+plot(exp(ener_sweden.dyn))
+lines(ener_sweden.ts)
+plot(ener_sweden.ts)
+dev.off()
 ##### VALIDATION #####
 # split x_train, y_train, x_val, y_val...80% and 20%
 
@@ -71,7 +85,25 @@ tsdisplay(residuals(ener_sweden.auto))
 # maybe use univariant for gdp, predict some values
 e <- rep(tail(ener_sweden.ts.s1, n=1), 10) # get 10 values 
 f <- forecast(ener_sweden.auto, h=10, xreg = ts(e)) # forecast
+f$mean<-exp(diffinv(f$mean,differences = 1,xi = log(5.511750)))
+f$x<-exp(diffinv(f$x,differences = 1,xi = log(2.700534)))
+a<-exp(diffinv(f$upper[,1],differences = 1,xi = log(5.511750)))
+a <-a[1:length(a)-1]
+length(a)
+f$upper[,1]<-a
+a<-exp(diffinv(f$upper[,1],differences = 1,xi = log(5.511750)))
+a <-a[1:length(a)-1]
+length(a)
+f$upper[,1]<-a
+
+
+
+f$upper[,1]<-exp(diffinv(f$upper[,1],differences = 1,xi = log(5.511750)))
+f$upper[,2]<-exp(diffinv(f$upper[,2],differences = 1,xi = log(5.511750)))
+f$lower[,1]<-exp(diffinv(f$lower[,1],differences = 1,xi = log(5.511750)))
+f$lower[,2]<-exp(diffinv(f$lower[,2],differences = 1,xi = log(5.511750)))
 plot(f) # plot
+
 
 ##### #####
 
